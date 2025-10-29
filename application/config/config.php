e92a34fefe3835612a7a9111cd76afd7bbf947cd
idasulistiana <idasulistiana@gmail.com>
2025-10-29 15:42:36 +0700

    change config database name

--- application/config/config.php
+++ application/config/config.php
@@ -530,3 +530,10 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+$db['default'] = array(
+    'hostname' => 'localhost',
+    'username' => 'binduk_user',
+    'password' => 'passwordku123',
+    'database' => 'binduk_db',
+    'dbdriver' => 'mysqli',
+);
