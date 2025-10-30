bb19983ea208994d37d87ac604ebf3f861680fe9
ida sulistiana <idasulistiana@gmail.com>
2025-10-30 20:39:55 +0700

    Update and rename tcpdf.php to Tcpdf.php

--- application/libraries/Tcpdf.php
+++ application/libraries/Tcpdf.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once APPPATH . 'third_party/tcpdf/tcpdf.php';
 
-class Pdf extends tcpdf {
+class Pdf extends TCPDF {
 
     public function __construct() {
         parent::__construct();
