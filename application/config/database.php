dabeb0fb875eee78fe440a6f2cc2dc66a850b285
ida sulistiana <idasulistiana@gmail.com>
2025-10-30 17:33:13 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,24 +73,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'u796341745_buku_induk04',
-	'password' => 'IdaSulistiana27@',
-	'database' => 'u796341745_buku_induk04',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => TRUE,
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
